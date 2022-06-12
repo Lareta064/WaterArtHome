d4ce32812aa23b6fd7a19abaa623c39cb318f16b
Lareta064 <lareta064@gmail.com>
2022-06-12 22:30:55 +0300

    services

--- src/js/main.js
+++ src/js/main.js
@@ -43,5 +43,34 @@ document.addEventListener("DOMContentLoaded", function (){
        });
       }
     }
-
+  /*============tabs ==================*/
+  function pageTabs(tabBtn, tabContent) {
+    const menuItems = tabBtn.querySelectorAll('[data-role]');
+    const menuItemContent = tabContent.querySelectorAll('[data-content]');
+    for (let i = 0; i < menuItems.length; i++) {
+      menuItems[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        for (let j = 0; j < menuItems.length; j++) {
+          if (j !== i) {
+            menuItems[j].classList.remove('active');
+          }
+          if (j == i) {
+            menuItems[j].classList.add('active');
+          }
+        }
+        const thisData = menuItems[i].getAttribute('data-role');
+        for (let block of menuItemContent) {
+          block.classList.remove('active');
+          const contentData = block.getAttribute('data-content');
+          if (thisData == contentData && menuItems[i].classList.contains('active')) {
+            block.classList.add('active');
+          }
+        }
+      });
+    }
+  }
+  const servicesTabs = document.getElementById('services-tabs');
+  if (servicesTabs) {
+  pageTabs(servicesTabs, servicesTabs);
+}
 });/*закрываем самую первую строчку, загрузка страницы */
