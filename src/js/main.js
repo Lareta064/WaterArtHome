4ff59fd76f174af6b4728005ef442892132af335
Lareta064 <lareta064@gmail.com>
2022-06-12 09:44:14 +0300

    кнопка на моб меню

--- src/js/main.js
+++ src/js/main.js
@@ -14,24 +14,34 @@ document.addEventListener("DOMContentLoaded", function (){
       mobileMenu.classList.remove('active');
       overlayBlock.classList.remove('active');
       bodyEl.classList.remove('noscroll');
-      
-
     } else {
         this.classList.add('active');
 	      mobileMenu.classList.add('active');
       	overlayBlock.classList.add('active');
       	bodyEl.classList.add('noscroll');
-    
       }
     });
+
+    /*======== закрывать моб меню при ресайзе экрана ====== */
     window.addEventListener('resize', function () {
       menuToggle.classList.remove('active');
       mobileMenu.classList.remove('active');
       overlayBlock.classList.remove('active');
       bodyEl.classList.remove('noscroll');
-      
-      
     });
 
 
-});
+    /*======== закрывать моб меню по клику на ссылку внутри этого меню ====== */
+    if(mobileMenu){
+      const mobMenuLinks = mobileMenu.querySelectorAll('a');
+      for(let item of mobMenuLinks){
+       item.addEventListener('click', function () {
+         menuToggle.classList.remove('active');
+         mobileMenu.classList.remove('active');
+         overlayBlock.classList.remove('active');
+         bodyEl.classList.remove('noscroll');
+       });
+      }
+    }
+
+});/*закрываем самую первую строчку, загрузка страницы */
