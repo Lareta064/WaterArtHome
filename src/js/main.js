4a1d5aedd529ce41f972f274240326cc66499d40
Lareta064 <lareta064@gmail.com>
2022-06-17 11:28:14 +0300

    fix tabs

--- src/js/main.js
+++ src/js/main.js
@@ -303,4 +303,61 @@ document.addEventListener("DOMContentLoaded", function (){
         });
       }
     }
+
+     /* modal*/ 
+    const modalFramesOpen = document.querySelectorAll('[frame-btn]');
+    const modalFrames = document.querySelectorAll('[frame-modal]');
+    if( modalFrames.length > 0){
+      
+      const modalFramesClose = document.querySelectorAll('[frame-close]');
+      for(let item of modalFramesOpen){
+        item.addEventListener('click', function(e){
+          for(let item of  modalFrames){
+            item.classList.remove('visible');
+
+            
+            bodyEl.classList.remove('noscroll');
+            overlay.classList.remove('active');
+            
+          }
+          e.preventDefault();
+          const itemAttr = item.getAttribute('frame-btn');
+
+          for(let frame of modalFrames){
+            const frameAttr =frame.getAttribute('frame-modal');	
+            if(frameAttr == itemAttr){
+              frame.classList.add('visible');
+              bodyEl.classList.add('noscroll');
+               overlay.classList.add('active');
+               if(mobileMenu.classList.contains('active')){
+                 mobileMenu.classList.remove('active');
+                menuToggle.classList.remove('active');
+               }
+            }
+          }
+        });
+      }
+      /*закрыть модалки с атрибутом frame-modal*/
+      for(let item of modalFramesClose){
+        item.addEventListener('click', function(e){
+          e.preventDefault();
+          item.closest('[frame-modal]').classList.remove('visible');
+          bodyEl.classList.remove('noscroll');
+          overlay.classList.remove('active');
+          
+        });
+      }
+    }
+    overlay.addEventListener('click', function(e){
+    for(let frame of modalFrames){
+      frame.classList.remove('visible');
+      
+      }
+      bodyEl.classList.remove('noscroll');
+      this.classList.remove('active');
+      
+    });
+
+
+
 });/*закрываем самую первую строчку, загрузка страницы */
